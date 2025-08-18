a19d7aab50cc5f41c789f522412e4063db3b3f2e
Arpan Narula <arpan_2403mc01@iitp.ac.in>
2025-08-18 19:23:32 +0530

    Update supportedLibraries.js

--- CTD25/ieat/supportedLibraries.js
+++ CTD25/ieat/supportedLibraries.js
@@ -1,18 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  navigator.clipboard.writeText("  D{           _connected}")
-    .catch(err => {
-      // Optional: inform the user to click a button if auto-copy fails
-      console.warn('Clipboard copy failed:', err);
-    });
-});
-
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('reload-copy-btn').addEventListener('click', function () {
+    document.getElementById('reload-btn').addEventListener('click', function () {
         const textToCopy = '  D{           _connected}';
         navigator.clipboard.writeText(textToCopy).then(function () {
             location.reload();
         }).catch(function (err) {
-            alert('Copy failed: ' + err + '\nPage will reload anyway.');
+            //alert('Copy failed: ' + err + '\nPage will reload anyway.');
             location.reload();
         });
     });
